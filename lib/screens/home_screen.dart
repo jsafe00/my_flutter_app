92ef8b289c1098203041ca105a42db554ea0a75f
Josafe Balili <josafebalili@gmail.com>
2024-05-16 16:56:13 +0800

    update in a stateful widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,40 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:my_flutter_app/screens/second_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String quote = "";
+  String author = "";
+  List<String> tagsText = [];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-     child: Scaffold(
-      body: Center(
-        child: ElevatedButton(
-        onPressed: () async {
-          var url = Uri.parse(
-              'https://api.quotable.io/random?tags=technology%2Cfamous-quotes');
-          var response = await http.get(url);
-          var data = jsonDecode(response.body);
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Quote Generator'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                quote,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  " - $author",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                ' ${tagsText.join(', ')}',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  var url = Uri.parse('https://api.quotable.io/random');
+                  var response = await http.get(url);
+                  var data = jsonDecode(response.body);
 
-          String content = data["content"];
-          String author = data["author"];
-          List<dynamic> tags = data["tags"];
-
-          List<String> tagsList = tags.map((tag) => tag.toString()).toList();
-
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => 
-              SecondScreen(
-                contentText: content, 
-                authorName: author,
-                tagsText: tagsList,
-            ))
-          );
-        }, 
-        child: Text("Get Quote!"),
-          ),
+                  setState(() {
+                    quote = data["content"];
+                    author = data["author"];
+                    tagsText = List<String>.from(data["tags"]);
+                  });
+                },
+                child: Text("Get Quote!"),
+              ),
+            ),
+          ],
         ),
       ),
     );
