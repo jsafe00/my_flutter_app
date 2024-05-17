17d6c020ed0466c180902d7791b98a2562683d3d
Josafe Balili <josafebalili@gmail.com>
2024-05-17 13:53:32 +0800

    simple stateful widget

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/screens/third_screen.dart';
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key}) : super(key: key);
+ void main(){
+  runApp(new SecondScreen());
+}
+
+
+class SecondScreen extends StatefulWidget {
+    @override
+  _ApplicationState createState() => new _ApplicationState();
+
+}
+
+class _ApplicationState extends State<SecondScreen>  {
+ 
+
+  String text = '';
+
+  @override
+  void initState() {
+    text = 'Click on this button';
+    super.initState();
+  }
+
+  void changedText(){
+    setState((){
+      text = 'The text is being changed.';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +38,23 @@ class SecondScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                child: Text("Go back"),
-                onPressed: () {
-                  Navigator.of(context).pop();
+                child: Text(text),
+                  onPressed: () {
+                    changedText();
                 },
               ),
               SizedBox(height: 20), 
               ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ThirdScreen()),
+                child: Text("Go back"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                },
+              ),
+                SizedBox(height: 20), 
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ThirdScreen()),
                   );
                 },
                 child: Text("Go to third screen"),
