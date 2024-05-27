883681296dbd46a37b88bd0b9aafb016c6df5f63
Josafe Balili <josafebalili@gmail.com>
2024-05-27 17:37:53 +0800

    routing init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/pages/choose_location.dart';
 import 'package:my_flutter_app/pages/home.dart';
+import 'package:my_flutter_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
